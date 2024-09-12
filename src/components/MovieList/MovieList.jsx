66c009fbcb65b89f215a21ff72bb6e95914f0174
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-09-12 15:27:19 +0300

    bug fixes

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import css from './MovieList.module.css'
 
 export default function MovieList({ movies }) {
-  if (!movies || movies.length === 0) {
-    return;
+  const location = useLocation();
+
+   if (!Array.isArray(movies) || movies.length === 0) {
+    return ; 
   }
 
   return (
     <ul className={css.ul}>
-      {movies.map(movie => (
-        <li  className={css.li} key={movie.id}>
-          <Link className={css.a}to={`/movies/${movie.id}`}>{movie.title}</Link>
+      {movies.map(({id, title}) => {
+        return(
+     <li  className={css.li} key={id}>
+        <Link className={css.a} to={`/movies/${id}`} state={{from: location}}>{title}</Link>
         </li>
-      ))}
+        );
+      })}
     </ul>
   );
 }
